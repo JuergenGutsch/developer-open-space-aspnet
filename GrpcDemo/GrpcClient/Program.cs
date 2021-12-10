837af88bacca86981abbbae9b7b0562f7ea6f50e
JeurgenGutsch <juergen@gutsch-online.de>
2021-12-10 16:25:15 +0100

    gRPC Server und Client

---
+++ GrpcDemo/GrpcClient/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Grpc.Net.Client;
+using GrpcDemo;
+
+// ACHTUNG: Die Demo benötigt den laufenden GrpcServer
+var channel = GrpcChannel.ForAddress("http://localhost:5000");
+var client = new Greeter.GreeterClient(channel);
+
+var response = await client.SayHelloAsync(new HelloRequest { Name = args[0] });
+
+// ACHTUNG: Diese Demo möchte ein argument. Also wie folgt aufrufen:
+// > dotnet run World
+Console.WriteLine($"{response.Message}!");
